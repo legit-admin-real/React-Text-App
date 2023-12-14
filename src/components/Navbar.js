ef1dec2752f51e8c0ae6783c638e56b2cb6a86d0
legit-admin-real <carrentalportal2@gmail.com>
2023-12-14 18:11:51 +0530

    removed href

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ function Navbar(props) {
         className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
       >
         <div className="container-fluid">
-          <a className="navbar-brand" href={ ()=> false}>
+          <a className="navbar-brand" href="#">
             {props.title}
           </a>
           <button
