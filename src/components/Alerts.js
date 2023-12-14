699eceddca80f7a5ac1e73ba416824c913457c97
legit-admin-real <carrentalportal2@gmail.com>
2023-12-14 20:37:55 +0530

    Fixed CLS for alert

--- src/components/Alerts.js
+++ src/components/Alerts.js
@@ -6,7 +6,7 @@ function Alerts(props) {
         return lower.charAt(0).toUpperCase() + lower.slice(1);
     }
     return (
-        <div style={{height: '50px'}}>
+        <div style={{height: '60px'}}>
         {props.alert && <div class={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
             <strong>{capitalize(props.alert.type)}</strong> : {props.alert.msg}
             {/* <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> */}
